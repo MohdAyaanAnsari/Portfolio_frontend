62db34c18b8694d2a7ecaf9668d9d14fb18fe96b
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-03-22 11:17:55 +0530

    fix: scroll to top on route change

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ScrollToTop from "./components/ScrollToTop";
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
@@ -7,6 +8,7 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+    <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>,
